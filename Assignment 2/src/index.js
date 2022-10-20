1821e7205b67f51d03db248214b4a93ae1a42258
Vincentius Janssen <janssen.vincentius@gmail.com>
2022-10-20 21:05:29 +0700

    assignment 2 complete

--- Assignment 2/src/index.js
+++ Assignment 2/src/index.js
@@ -1,95 +1,208 @@
-window.addEventListener('load', ()=>{
-    document.addEventListener('click', click);    
-    document.addEventListener('mousedown', startdrawormove);
-    document.addEventListener('mouseup', stopdrawormove);
-    document.addEventListener('mousemove', sketch);
-});
-
 var canvas = new fabric.Canvas("canvas");
-var isDown, origX, origY;
+var isDown, origX, origY, circle, rectangle, triangle;
 
-var colorinput = document.getElementById("favcolor");
+//var colorinput = document.getElementById("favcolor");
 var slider = document.getElementById("opacity");
-var output_o = document.getElementById("value");
-var colorvalue = document.getElementById('colorVal');
+var output_o = document.getElementById("slidervalue");
+var colorinput = document.getElementById("color-select");
+var shape = document.getElementById("shape-select")
 
 output_o.innerHTML = slider.value;
 colorvalue.innerHTML = colorinput.value;
 
-var tooltype = "draw";
+var tooltype = "draw"; // Set default to draw
 
 
-function click(event){ 
-  // if users click reset, clear all shapes in the interface (10pts)
 
-  /* your code is here */
 
-}
-
-function startdrawormove(event) {
+canvas.on('mouse:down', function (o) {
   isDown = true;
-    var pointer = canvas.getPointer(event); // get mouse position
-    origX = pointer.x;
-    origY = pointer.y;
-    if (tooltype=="draw"){
-      //Use fabric.Circle/Rect/Triangle to define a circle/rectangle/triangle, respectively. Each shape is for 9pts
-     /* your code is here */
+  var pointer = canvas.getPointer(o.e);
+  origX = pointer.x;
+  origY = pointer.y;
+
+  if (tooltype == 'draw') {
+
+    // Universal properties
+    var colorFill = colorinput.value;
+    var colorStroke = '#e0e0e0';
+    var widthStroke = 3;
+
+    // Use fabric.Circle/Rect/Triangle to define a circle/rectangle/triangle, respectively. Each shape is for 9pt
+    circle = new fabric.Circle({
+      left: origX,
+      top: origY,
+      originX: 'left',
+      originY: 'top',
+      radius: pointer.x - origX,
+      angle: 0,
+      fill: colorFill,
+      stroke: colorStroke,
+      strokeWidth: widthStroke,
+      opacity: slider.value
+    });
+
+    rectangle = new fabric.Rect({
+      left: origX,
+      top: origY,
+      originX: 'left',
+      originY: 'top',
+      width: pointer.x - origX,
+      height: pointer.y - origY,
+      fill: colorFill,
+      stroke: colorStroke,
+      strokeWidth: widthStroke,
+      opacity: slider.value
+    });
+
+    triangle = new fabric.Triangle({
+      width: pointer.x - origX,
+      height: pointer.y - origY,
+      fill: colorFill,
+      left: origX,
+      top: origY,
+      originX: 'left',
+      originY: 'top',
+      stroke: colorStroke,
+      strokeWidth: widthStroke,
+      opacity: slider.value
+    });
 
     // add the defined shape into canvas (3pts).
-    /* your code is here */
-
+    switch (shape.value) {
+      case 'circle':
+        canvas.add(circle);
+        break;
+      case 'rectangle':
+        canvas.add(rectangle);
+        break;
+      case 'triangle':
+        canvas.add(triangle);
+        break;
     }
-    else if (tooltype=="move"){
-      // make all shapes selectable (4pts).
-    /* your code is here */
-    }
-    
-}
+  }
+});
 
-function stopdrawormove(event){
+canvas.on('mouse:move', function (o) {
+  if (tooltype == 'draw') {
+    if (!isDown) return;
+    var pointer = canvas.getPointer(o.e);
+
+    // Circle handling
+    if (shape.value == 'circle') {
+      var radius = Math.max(Math.abs(origY - pointer.y), Math.abs(origX - pointer.x)) / 2;
+      //if (radius > circle.strokeWidth) {radius -= circle.strokeWidth / 2;}
+      circle.set({ radius: radius });
+
+      if (origX > pointer.x) {
+        circle.set({ originX: 'right' });
+      } else {
+        circle.set({ originX: 'left' });
+      }
+      if (origY > pointer.y) {
+        circle.set({ originY: 'bottom' });
+      } else {
+        circle.set({ originY: 'top' });
+      }
+      circle.setCoords();
+    }
+
+    // Rectangle handling
+    if (shape.value == 'rectangle') {
+      var width = Math.abs(origX - pointer.x);
+      var height = Math.abs(origY - pointer.y);
+      rectangle.set({ width: width, height: height });
+
+      if (origX > pointer.x) {
+        rectangle.set({ originX: 'right' });
+      } else {
+        rectangle.set({ originX: 'left' });
+      }
+      if (origY > pointer.y) {
+        rectangle.set({ originY: 'bottom' });
+      } else {
+        rectangle.set({ originY: 'top' });
+      }
+      rectangle.setCoords();
+    }
+
+    // Triangle handling
+    if (shape.value == 'triangle') {
+      var width = Math.abs(origX - pointer.x);
+      var height = Math.abs(origY - pointer.y);
+      triangle.set({ width: width, height: height });
+
+      if (origY > pointer.y) {
+        triangle.set({ originY: 'bottom', angle: 0 });
+        if (origX > pointer.x) {
+          triangle.set({ originX: 'right' });
+        } else {
+          triangle.set({ originX: 'left' });
+        }
+      } else {
+        triangle.set({ originY: 'bottom', angle: 180 });
+        if (origX > pointer.x) {
+          triangle.set({ originX: 'left' });
+        } else {
+          triangle.set({ originX: 'right' });
+        }
+      }
+      triangle.setCoords()
+    }
+
+    // Make all objects unselectable
+    /* 
+     * This has to be located at the end of the draw function as the last drawn item is automatically selected
+     * To prevent that, disable selection after object is drawn.
+     */
+    canvas.selection = false;
+    canvas.forEachObject(function (object) {
+      object.selectable = false;
+      object.hasBorders = false;
+      object.hasControls = false;
+    });
+    canvas.requestRenderAll();
+
+  }
+
+});
+
+canvas.on('mouse:up', function (o) {
   isDown = false;
-}
+});
 
-function sketch(event){
-  if (tooltype=="draw"){
-  if (!isDown) return;
-    var pointer = canvas.getPointer(event); 
-    if (shapetype == 'Circle'){
-      // set the circle radius based on the mouse position (6pts)
-      /* your code is here */
-    }
-    else if (shapetype == 'Rect') {
-      // set the width and height of rectangle or triangle based on the mouse position (6pts)
-      /* your code is here */
-    }
-    
-  }
-  else if (tooltype == "move"){
-    var pointer = canvas.getPointer(event);
 
-    // move the selected shape  hint: use getActiveObject() function(8pts)
-    /* your code is here */
-  }
-
-  // get all shapes from canvas (6pts) and change the opacity of each shape (6pts)
-
-  /* your code is here */
-}
-
-function select_shape(shape){
-  shapetype = shape.value;
-}
-
-function use_tool(tool){
+// Helper functions
+function use_tool(tool) {
   tooltype = tool;
+
+  // Enable move -> Make all objects selectable
+  if (tooltype == "move") {
+    canvas.selection = true;
+    canvas.forEachObject(function (object) {
+      object.selectable = true;
+      object.hasBorders = true;
+      object.hasControls = true;
+    });
+    canvas.requestRenderAll();
+
+  }
 }
 
-slider.oninput = function() {
+function reset() {
+  // if users click reset, clear all shapes in the interface (10pts)
+  canvas.clear();
+}
+
+// Input Handling
+slider.oninput = function () {
   output_o.innerHTML = this.value;
+  canvas.forEachObject(function (object) {
+    object.set('opacity', slider.value)
+    canvas.requestRenderAll();
+  });
 }
 
-colorinput.oninput = function() {
-  colorvalue.innerHTML = this.value;
+colorinput.oninput = function () {
+  colorvalue.innerHTML = this.value; //cannot have hyphens in js variables
 }
-
-
